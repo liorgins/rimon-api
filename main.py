91b7dc3f5506d29d3c90a4f6601ff0b0950a70d1
Ziv Ginsberg <zivgin@gmail.com>
2025-05-25 16:57:25 +0400

    feat: run delta_calculation after extraction in main.py for automatic Delta/csv and Delta/json creation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from rimon_data_extractor import run_full_extraction, LEVEL_MAP
+from delta_calculation import create_delta_structure
 
 API_URL = "https://rimonapi.weevi.com/api/ekomcategories/md_GetStaticData?searchKey=categories&preventcaching=true&returnasstring=false"
 # Set as 'INFO' or 'DEBUG'
@@ -7,6 +8,7 @@ VERBOSITY_LEVEL = 'INFO'
 
 def main():
     run_full_extraction(API_URL, LEVEL_MAP[VERBOSITY_LEVEL])
+    create_delta_structure()
 
 if __name__ == "__main__":
     main() 
